37accdecb415f74a18e160401f37e1985d99f9ee
Pouya Ebrahimi <pouyaebrahimi1379@gmail.com>
2024-10-25 18:44:15 +0330

    password_generator: defining class for generating random_password

--- 03-Password_Generator/src/main.py
+++ 03-Password_Generator/src/main.py
@@ -24,6 +24,38 @@ class PinCodeGenerator(PasswordGenerator):
         return ''.join(random.choice(string.digits) for i in range(self.length))
     
 
+class RandomPasswordGenerator(PasswordGenerator):
+    """Class to generate a random password
+    """
+    def __init__(
+        self, 
+        length: int=8, 
+        include_numbers: bool=False, 
+        include_symbols: bool=False
+        ) -> None:
+        
+        self.length = length
+        self.charcters = string.ascii_letters
+        if include_numbers:
+            self.charcters += string.digits
+        if include_symbols:
+            self.charcters += string.punctuation
+
+    def generate(self) -> str:
+        """Method to generate a password
+
+        :return: random password
+        """
+        return ''.join(random.choice(self.charcters) for i in range(self.length))
+    
+
 if __name__ == '__main__':
     p1 = PinCodeGenerator(length=10)
     print(f'Your password is: {p1.generate()}')
+
+    p2 = RandomPasswordGenerator(
+        length=16, 
+        include_numbers=True, 
+        include_symbols=True
+        )
+    print(f'Your password is: {p2.generate()}')
